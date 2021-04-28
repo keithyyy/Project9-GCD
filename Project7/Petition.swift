8933f8d98b516371b4fb9889e829a6b9e0572e82
Keith Crooc <keith.crooc@gmail.com>
2021-04-27 22:03:28 -0400

    end of project 7, to add challenge components

---
+++ Project7/Petition.swift
@@ -0,0 +1,16 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by Keith Crooc on 2021-04-26.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
+
+
---
+++ Project7/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by Keith Crooc on 2021-04-26.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
